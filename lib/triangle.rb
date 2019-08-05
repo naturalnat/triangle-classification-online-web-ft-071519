ff7b96dc3e713fd35dccfc56329c86d3c749bc06
Natalie Taktachev <ntaktach@gmail.com>
2019-08-05 08:04:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
 
   def real_triangle
     if (a + b > c) || (b + c > a) || (a + c > b)
-      true  
+      true
      else
         [a, b, c].each do |side|
           raise TriangleError if side <= 0
