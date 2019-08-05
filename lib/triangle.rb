e94f3bc77a939916a5862b6968ddea7826daf739
Natalie Taktachev <ntaktach@gmail.com>
2019-08-05 07:34:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
   end
 
   def kind
-    if (a + b > c) || (b + c > a) || (a + c > b)
+    if (a + b > c) || (b + c > a) || (a + c > b) 
+      && a != 0 && b != 0 && c != 0 
 
       then
         if a == b && b == c
