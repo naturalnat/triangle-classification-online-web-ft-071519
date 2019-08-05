86e2f16cfe97ca8e7a12852ae7076b4d0566b8e1
Natalie Taktachev <ntaktach@gmail.com>
2019-08-05 08:24:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,7 @@ class Triangle
   end
 
 def kind
-  if (a + b > c) || (b + c > a) || (a + c > b) == false
-
-  if a <= 0 || b <= 0 || c <= 0
+  if (a + b < c) || (b + c < a) || (a + c < b) || a <= 0 || b <= 0 || c <= 0
         raise TriangleError
     elsif a == b && b == c
       :equilateral
